f943413e49ea99c5482a2f6a5084180a243c0878
patilchirag41 <31428820+patilchirag41@users.noreply.github.com>
2024-08-02 18:18:11 +0530

    initial commit

---
+++ src/main/java/Main_Framework/UIAutomation/app/Calculator.java
@@ -0,0 +1,7 @@
+package Main_Framework.UIAutomation.app;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/Main_Framework/UIAutomation/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,9 @@
+package Main_Framework.UIAutomation.acceptancetests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/", glue="Main_Framework.UIAutomation")
+public class AcceptanceTestSuite {}
